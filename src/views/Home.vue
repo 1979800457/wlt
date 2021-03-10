bcf08f4847e97b1a1ede2489b3360e9ac8d74aba
1979800457 <1979800457@qq.com>
2021-03-10 10:27:45 +0800

    第一次更改

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="home">
+   
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to='csc'>
+      <button>登录页面
+      </button>
+    </router-link>
+    <pd/>
+    </div>
+</template>
+
+<script>
+import csc from './csc'
+export default {
+  components:{
+   pd:csc
+  },
+  props:{},
+  data(){
+    return {
+    }
+  },
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style scoped>
+</style>
---
+++ src/views/csc.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="cc">
+    <p>首页</p>
+    <p>活动</p>
+    <p>新闻</p>
+    <p>动态</p>
+    <p>个人中心</p>
+</div>
+</template>
+
+<script>
+export default {
+  components:{},
+  props:{},
+  data(){
+    return {
+    }
+  },
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style scoped>
+.cc{
+    background-color: rgb(212, 121, 36);
+    color: rgb(30, 218, 61);
+    display: flex;
+    justify-content: space-around;
+    
+}
+</style>
